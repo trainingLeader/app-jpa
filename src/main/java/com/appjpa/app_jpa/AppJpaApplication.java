12848234958c5d9180f1f13391954d4671ae7f7e
darksidePardo <jjpardo2002@gmail.com>
2024-07-23 19:02:05 -0500

    feat: :tada: Create project
    Create project using JPA

---
+++ src/main/java/com/appjpa/app_jpa/AppJpaApplication.java
@@ -0,0 +1,20 @@
+package com.appjpa.app_jpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJpaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'run'");
+	}
+
+}
---
+++ src/test/java/com/appjpa/app_jpa/AppJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appjpa.app_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

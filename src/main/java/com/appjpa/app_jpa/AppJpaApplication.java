f8117276b3c04512dc74517f50aba6cba0d9a98e
darksidePardo <jjpardo2002@gmail.com>
2024-07-24 07:09:38 -0500

    feat: :sparkles: New features
    New features

--- src/main/java/com/appjpa/app_jpa/AppJpaApplication.java
+++ src/main/java/com/appjpa/app_jpa/AppJpaApplication.java
@@ -1,6 +1,7 @@
 package com.appjpa.app_jpa;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -54,6 +55,31 @@ public class AppJpaApplication implements CommandLineRunner {
 
 		personRepository.findById(personNew.getId()).ifPresent(System.out::println);
 
+	}
+	@Transactional
+	public void update() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona:");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = personRepository.findById(id);
+
+		// optionalPerson.ifPresent(person -> {
+		if (optionalPerson.isPresent()) {
+			Person personDB = optionalPerson.orElseThrow();	
+
+			System.out.println(personDB);
+			System.out.println("Ingrese el lenguaje de programacion:");
+			String programmingLanguage = scanner.next();
+			personDB.setProgrammingLanguage(programmingLanguage);
+			Person personUpdated = personRepository.save(personDB);
+			System.out.println(personUpdated);
+		} else {
+			System.out.println("El usuario no esta presente! no existe!");
+		}
+
+		scanner.close();
 	}
 
 }
